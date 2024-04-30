e2c75645434f51d9a27a2bc502a133afb2ca34bb
Aleksandar Alempic <88376453+AleksandarAlempic@users.noreply.github.com>
2024-04-30 17:59:55 +0200

    Addes Script and Images folders

---
+++ script/Data.js
@@ -0,0 +1,122 @@
+
+let songs = [
+   {
+      name: 'Alive',
+      path: 'audio/Sia - Alive (Official Video).mp3',
+      artist: 'Sia',
+      cover: 'images/audio/Alive.jpg'
+   },
+   
+   {
+      name: 'Wavin Flag',
+      path: 'audio/Wavin Flag (Spanish Version) - Knaan & David Bisbal - World Cup 2010 theme song (128 kbps).mp3',
+      artist: 'Knaan & David Bisbal',
+      cover: 'images/audio/World Cup 2010 theme song.jpg'
+   },
+
+
+   {
+      name: 'We Are One',
+      path: 'audio/We Are One (Ole Ola) [The Official 2014 FIFA World Cup Song].mp3',
+      artist: 'Pitbull feat J.Lopez&C.Leitte ',
+      cover: 'images/audio/WeAreOne.jpg'
+   },
+   {
+      name: 'La la la',
+      path: 'audio/Shakira - La La La (Brazil 2014) ft. Carlinhos Brown.mp3',
+      artist: 'Shakira ft. Carlinhos Brown',
+      cover: 'images/audio/Shakira.jpg'
+   },
+ 
+   {
+      name: 'Walk',
+      path: 'audio/Kwabs - Walk (Official Video).mp3',
+      artist: 'Kwabs',
+      cover: 'images/audio/Kwabs.jpg'
+   },
+   
+   {
+      name: 'Hall of Fame',
+      path: 'audio/The Script - Hall of Fame (Official Video) ft. will.i.am.mp3',
+      artist: 'The Script - ft. will.i.am',
+      cover: 'images/audio/HallOfFame.jpg '
+   },
+
+   {      name: 'Superheroes',
+      path: 'audio/The Script - Superheroes (Official Video).mp3',
+      artist: 'The Script',
+      cover: 'images/audio/TheScript.jpg'
+   },
+
+   {
+      name: 'Alone (Solo)',
+      path: 'audio/Alone (Solo).mp3',
+      artist: 'IL DIVO',
+      cover: 'images/audio/Alone.jpg'
+   },
+ 
+   {
+      name: 'The Power Of Love',
+      path: 'audio/IL DIVO - The Power Of Love (La Fuerza Mayor) (Live Video).mp3',
+      artist: 'IL DIVO',
+      cover: 'images/audio/IlDivo.jpg'
+    },
+    {
+      name: 'Out of Touch',
+      path: 'audio/Hall & Oates  - Out of Touch.mp3',
+      artist: 'Hall & Oates',
+      cover: 'images/audio/Hall & Oates.jpg'
+   }, 
+
+   {
+      name: 'Say It Right',
+      path: 'audio/Nelly Furtado - Say It Right (Official Music Video).mp3',
+      artist: 'Nelly Furtado',
+      cover: 'images/audio/NellyFurtado.jpg'
+   }, 
+
+   {
+      name: 'Calling On Me',
+      path: 'audio/Sean Paul, Tove Lo - Calling On Me (128 kbps).mp3',
+      artist: 'Sean Paul',
+      cover: 'images/audio/SeanPaul.jpg'
+   },
+
+   {
+      name: 'Vracaju se dani pobede',
+      path: 'audio/Grupa JNA - Vracaju se dani pobede.mp3',
+      artist: '',
+      cover: 'images/audio/GrupaJNA.jpg'
+   },
+
+    {
+      name: 'Gimme! Gimme!',
+      path: 'audio/ABBA - Gimme! Gimme! Gimme! (A Man After Midnight).mp3',
+      artist: 'ABBA',
+      cover: 'images/audio/ABBA-GimmeGimmeGimme-video.png'
+    },  
+
+   {
+      name: 'Jutro je',
+      path: 'audio/Nada Topcagic - Jutro je - (Audio 1990) HD (128 kbps).mp3',
+      artist: 'Nada Topcagic',
+      cover: 'images/audio/NadaTopcagic.jpg'
+   },
+
+   {
+      name: 'U inat proslosti',
+      path: 'audio/Aleksandra Radovic & The Beatshakers - Who Gonna Stop Me Now (Official HD Video).mp3',
+      artist: 'Aleksandra Radovic',
+      cover: 'images/audio/AleksandraRadovic.jpg'
+   },
+ 
+   {
+      name: 'Ne Pitaj',
+      path: 'audio/LegendE _ Ne pitaj - (LIVE) - (Sava centar 2005) (128 kbps).mp3',
+      artist: 'Legende',
+      cover: 'images/audio/LegendeNePitaj.jpg'
+   },
+
+   
+
+]
