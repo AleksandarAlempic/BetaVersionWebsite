0e9e789ab72d303ca4d7f382425750eace587602
Aleksandar Alempic <88376453+AleksandarAlempic@users.noreply.github.com>
2024-05-03 23:18:28 +0200

    New Songs update Script Data.js

--- script/Data.js
+++ script/Data.js
@@ -1,12 +1,33 @@
 
 let songs = [
+
    {
-      name: 'Alive',
-      path: 'audio/Sia - Alive (Official Video).mp3',
-      artist: 'Sia',
-      cover: 'images/audio/Alive.jpg'
+      name: 'Alone (Solo)',
+      path: 'audio/Alone (Solo).mp3',
+      artist: 'IL DIVO',
+      cover: 'images/audio/Alone.jpg'
    },
-   
+
+   {
+      name: 'The Power Of Love',
+      path: 'audio/IL DIVO - The Power Of Love (La Fuerza Mayor) (Live Video).mp3',
+      artist: 'IL DIVO',
+      cover: 'images/audio/IlDivo.jpg'
+    },
+  
+   {
+      name: 'Waka waka',
+      path: 'audio/Waka Waka (Esto es Africa).mp3',
+      artist: 'Shakira',
+      cover: 'images/audio/WakaWaka.jpg'
+   },
+   {
+      name: 'We Are One',
+      path: 'audio/We Are One (Ole Ola) [The Official 2014 FIFA World Cup Song].mp3',
+      artist: 'Pitbull feat J.Lopez&C.Leitte ',
+      cover: 'images/audio/WeAreOne.jpg'
+   },
+
    {
       name: 'Wavin Flag',
       path: 'audio/Wavin Flag (Spanish Version) - Knaan & David Bisbal - World Cup 2010 theme song (128 kbps).mp3',
@@ -14,13 +35,6 @@ let songs = [
       cover: 'images/audio/World Cup 2010 theme song.jpg'
    },
 
-
-   {
-      name: 'We Are One',
-      path: 'audio/We Are One (Ole Ola) [The Official 2014 FIFA World Cup Song].mp3',
-      artist: 'Pitbull feat J.Lopez&C.Leitte ',
-      cover: 'images/audio/WeAreOne.jpg'
-   },
    {
       name: 'La la la',
       path: 'audio/Shakira - La La La (Brazil 2014) ft. Carlinhos Brown.mp3',
@@ -34,7 +48,42 @@ let songs = [
       artist: 'Kwabs',
       cover: 'images/audio/Kwabs.jpg'
    },
-   
+  
+   {
+      name: 'Jutro je',
+      path: 'audio/Nada Topcagic - Jutro je - (Audio 1990) HD (128 kbps).mp3',
+      artist: 'Nada Topcagic',
+      cover: 'images/audio/NadaTopcagic.jpg'
+   },
+
+   {
+      name: 'U inat proslosti',
+      path: 'audio/Aleksandra Radovic & The Beatshakers - Who Gonna Stop Me Now (Official HD Video).mp3',
+      artist: 'Aleksandra Radovic',
+      cover: 'images/audio/AleksandraRadovic.jpg'
+   },
+
+   {
+      name: 'Devet Zivota',
+      path: 'audio/ALEKSANDRA PRIJOVIC - DEVET ZIVOTA (OFFICIAL VIDEO).mp3',
+      artist: 'Aleksandra Prijovic',
+      cover: 'images/audio/AleksandraPrijovic.jpg'
+   },
+
+   {
+      name: 'Vracaju se dani pobede',
+      path: 'audio/Grupa JNA - Vracaju se dani pobede.mp3',
+      artist: '',
+      cover: 'images/audio/GrupaJNA.jpg'
+   },
+
+   {
+      name: 'Ne Pitaj',
+      path: 'audio/LegendE _ Ne pitaj - (LIVE) - (Sava centar 2005) (128 kbps).mp3',
+      artist: 'Legende',
+      cover: 'images/audio/LegendeNePitaj.jpg'
+   },
+
    {
       name: 'Hall of Fame',
       path: 'audio/The Script - Hall of Fame (Official Video) ft. will.i.am.mp3',
@@ -49,24 +98,11 @@ let songs = [
    },
 
    {
-      name: 'Alone (Solo)',
-      path: 'audio/Alone (Solo).mp3',
-      artist: 'IL DIVO',
-      cover: 'images/audio/Alone.jpg'
-   },
- 
-   {
-      name: 'The Power Of Love',
-      path: 'audio/IL DIVO - The Power Of Love (La Fuerza Mayor) (Live Video).mp3',
-      artist: 'IL DIVO',
-      cover: 'images/audio/IlDivo.jpg'
-    },
-    {
       name: 'Out of Touch',
       path: 'audio/Hall & Oates  - Out of Touch.mp3',
       artist: 'Hall & Oates',
       cover: 'images/audio/Hall & Oates.jpg'
-   }, 
+   },
 
    {
       name: 'Say It Right',
@@ -74,49 +110,27 @@ let songs = [
       artist: 'Nelly Furtado',
       cover: 'images/audio/NellyFurtado.jpg'
    }, 
-
+   
    {
       name: 'Calling On Me',
       path: 'audio/Sean Paul, Tove Lo - Calling On Me (128 kbps).mp3',
       artist: 'Sean Paul',
       cover: 'images/audio/SeanPaul.jpg'
    },
-
+ 
    {
-      name: 'Vracaju se dani pobede',
-      path: 'audio/Grupa JNA - Vracaju se dani pobede.mp3',
-      artist: '',
-      cover: 'images/audio/GrupaJNA.jpg'
-   },
-
-    {
       name: 'Gimme! Gimme!',
       path: 'audio/ABBA - Gimme! Gimme! Gimme! (A Man After Midnight).mp3',
       artist: 'ABBA',
       cover: 'images/audio/ABBA-GimmeGimmeGimme-video.png'
-    },  
+    },
+
 
    {
-      name: 'Jutro je',
-      path: 'audio/Nada Topcagic - Jutro je - (Audio 1990) HD (128 kbps).mp3',
-      artist: 'Nada Topcagic',
-      cover: 'images/audio/NadaTopcagic.jpg'
+      name: 'Alive',
+      path: 'audio/Sia - Alive (Official Video).mp3',
+      artist: 'Sia',
+      cover: 'images/audio/Alive.jpg'
    },
-
-   {
-      name: 'U inat proslosti',
-      path: 'audio/Aleksandra Radovic & The Beatshakers - Who Gonna Stop Me Now (Official HD Video).mp3',
-      artist: 'Aleksandra Radovic',
-      cover: 'images/audio/AleksandraRadovic.jpg'
-   },
- 
-   {
-      name: 'Ne Pitaj',
-      path: 'audio/LegendE _ Ne pitaj - (LIVE) - (Sava centar 2005) (128 kbps).mp3',
-      artist: 'Legende',
-      cover: 'images/audio/LegendeNePitaj.jpg'
-   },
-
    
-
 ]
