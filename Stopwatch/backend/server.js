308f5c7495faa53c497878420f144fcb5eb8a0a6
Aleksandar Alempic <88376453+AleksandarAlempic@users.noreply.github.com>
2026-01-19 10:44:42 +0100

    Update server.js

--- Stopwatch/backend/server.js
+++ Stopwatch/backend/server.js
@@ -29,6 +29,8 @@ const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
+require("dotenv").config();
+
 // ---------------- API ROUTES ---------------- //
 
 // Snimanje trčanja
