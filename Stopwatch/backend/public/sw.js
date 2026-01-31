20d479bbeee1f7166ae9097e760a334e1041dfbf
Aleksandar Alempic <88376453+AleksandarAlempic@users.noreply.github.com>
2026-01-31 18:30:37 +0100

    Update sw.js

--- Stopwatch/backend/public/sw.js
+++ Stopwatch/backend/public/sw.js
@@ -1,1 +1,46 @@
+const CACHE_NAME = "kingsvillage-fit-v1";
 
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/Stopwatch.css",
+  "/stylesResponsive.css",
+  "/javascript.js",
+  "/manifest.json"
+];
+
+// INSTALL
+self.addEventListener("install", event => {
+  console.log("🛠 Service Worker installing...");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// ACTIVATE
+self.addEventListener("activate", event => {
+  console.log("✅ Service Worker activated");
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// FETCH
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
