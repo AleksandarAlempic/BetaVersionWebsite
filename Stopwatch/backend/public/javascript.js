c9f14fd4b0bcd50bd980ae475d839accba41df05
Aleksandar Alempic <88376453+AleksandarAlempic@users.noreply.github.com>
2026-03-17 17:49:39 +0000

    Update javascript.js 2

--- Stopwatch/backend/public/javascript.js
+++ Stopwatch/backend/public/javascript.js
@@ -714,7 +714,8 @@ routes.forEach(route => {
   const marker = L.marker(latlngs[0], {
     icon: runnerIcon,
     interactive: true,
-    pane: 'markersPane'
+    pane: 'markersPane', 
+   zIndexOffset: 1000  // Dodajemo zIndexOffset da marker bude iznad drugih slojeva
   }).addTo(map);
 
   marker.options.routeData = route;
